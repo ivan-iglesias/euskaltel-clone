00d4a5600d13793718ca68cdfd9b673320578151
Ivan Iglesias <iiglesiasbilbao@gmail.com>
2023-02-28 18:31:50 +0100

    Add coverage banner

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import TheHeader from '@/components/TheHeader.vue';
 import TheMobilePlans from '@/components/TheMobilePlans.vue';
 import TheInternetMobileBundles from '@/components/TheInternetMobileBundles.vue';
+import TheCoverageBanner from '@/components/TheCoverageBanner.vue';
 import BaseCarrousel from '@/components/base/BaseCarrousel.vue';
 import homeSliders from '@/data/HomeSliders.json';
 </script>
@@ -12,6 +13,7 @@ import homeSliders from '@/data/HomeSliders.json';
 
   <main class="main">
     <BaseCarrousel :sliders="homeSliders" />
+    <TheCoverageBanner />
     <TheMobilePlans />
     <TheInternetMobileBundles />
   </main>
---
+++ src/components/TheCoverageBanner.vue
@@ -0,0 +1,85 @@
+<script setup>
+import BaseButton from "@/components/base/BaseButton.vue";
+import IconPin from '@/components/icons/IconPin.vue';
+
+const onClick = () => console.log(`Coverage feature`);
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <section class="banner-coverage">
+      <div class="banner-coverage__container">
+        <IconPin class="banner-coverage__icon" />
+        <div><strong>¿Puedes tener Euskaltel en tu casa?</strong></div>
+        <BaseButton
+          @on-click-button="onClick()"
+          class="banner-coverage__button"
+        >Compruébalo</BaseButton>
+      </div>
+    </section>
+  </div>
+
+  <!-- <div class="wrapper">
+    <section class="banner-coverage">
+      <div class="banner-coverage__container">
+        <IconPin class="banner-coverage__icon" />
+        <div><strong>¿Puedes tener Euskaltel en tu casa?</strong></div>
+        <BaseButton
+          @on-click-button="onClick()"
+          class="banner-coverage__button"
+        >Compruébalo</BaseButton>
+      </div>
+    </section>
+  </div> -->
+</template>
+
+<style lang="scss">
+.banner-coverage {
+  background-color: var(--color-primary-light);
+  border-radius: var(--border-radius);
+  margin: 2rem 0;
+  padding: 1.3rem;
+
+  &::before {
+    background-image: url('/images/coverage.svg');
+    background-repeat: no-repeat;
+    background-size: auto 100%;
+    content: "";
+    display: block;
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 64%;
+    -webkit-mask-image: linear-gradient(270deg, transparent 25%, #000 75%);
+    mask-image: linear-gradient(270deg, transparent 25%, #000 75%);
+
+    @include for-tablet() {
+      width: 32%;
+    }
+  }
+
+  &__container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    @include for-tablet() {
+      flex-direction: row;
+      gap: 2rem;
+    }
+  }
+
+  &__icon {
+    height: 64px;
+    width: 64px;
+  }
+
+  &__button {
+    width: 150px;
+  }
+}
+</style>
